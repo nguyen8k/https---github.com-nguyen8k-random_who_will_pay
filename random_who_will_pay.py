ad853c10227fd3a35a397d46e90b01fa425e689f
nguyen8k <56096127+nguyen8k@users.noreply.github.com>
2021-04-24 01:08:15 -0600

    first commit

---
+++ random_who_will_pay.py
@@ -0,0 +1,15 @@
+import random
+
+names_string = input (" Give me everybody's names, seperated by a comma : \n")
+names = names_string.split(",")
+num_items = len(names)
+
+print (names)
+print(len(names))
+
+random_choice = random.randint(0, num_items -1)
+
+print(random_choice)
+
+person_will_pay = names [random_choice]
+print (f"The person will pay : {person_will_pay}")
